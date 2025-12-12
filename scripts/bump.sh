73138e7c2e8fe2406b8c631c457191e90acad7d3
Matt Long <ml@medplum.com>
2025-12-12 10:06:20 -0800

    Update bump script and publish.yml

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -5,12 +5,6 @@ NEW_VERSION=$1
 
 echo "Bumping to v$NEW_VERSION"
 
-echo $NEW_VERSION > version.txt
-git add version.txt
-
-sed -i '' -E -e "s/VERSION = '[^\']*'/VERSION = '$NEW_VERSION'/g" version.js
-git add version.js
-
 sed -i '' -E -e "s/\"version\": \"[^\"]*\"/\"version\": \"$NEW_VERSION\"/g" package.json
 git add package.json
 
