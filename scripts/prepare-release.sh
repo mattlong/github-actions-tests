f28219c7fa84d2b65062733c264fc7c6dff81029
Matt Long <ml@medplum.com>
2025-12-12 11:47:56 -0800

    Add prepare release

---
+++ scripts/prepare-release.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+# Fail on error
+set -e
+
+# Echo commands
+set -x
+
+# Get the current version number
+CURR_VERSION=$(node -p "require('./package.json').version")
+
+# Convert version string to array using '.' as delimiter
+IFS='.' read -ra CURR_VERSION_PARTS <<< "$CURR_VERSION"
+
+if [[ -z "${GITHUB_REF_NAME}" ]]; then
+  echo "GITHUB_REF_NAME is missing"
+  exit 1
+fi
+
+if [[ -z "${IS_LATEST}" ]]; then
+  echo "IS_LATEST is missing"
+  exit 1
+fi
+
+if [[ "${IS_LATEST}" != "true" && "${IS_LATEST}" != "false" ]]; then
+  echo "IS_LATEST must be 'true' or 'false'"
+  exit 1
+fi
+
+# By default, increase the patch version
+((CURR_VERSION_PARTS[2]++))
+
+# Build the new version number
+NEW_VERSION="${CURR_VERSION_PARTS[0]}.${CURR_VERSION_PARTS[1]}.${CURR_VERSION_PARTS[2]}"
+
+# Check if a new version is provided as a command line argument and override if present
+if [ ! -z "$1" ]; then
+    NEW_VERSION=$1
+    echo "Overriding new version to: $NEW_VERSION"
+else
+    echo "New version: $NEW_VERSION"
+fi
+
+# Use the Github gh tool to make sure the user is logged in
+gh auth status
+
+# Start a new branch
+BRANCH_NAME="version-$NEW_VERSION"
+git checkout -b "$BRANCH_NAME"
+
+# Set version in package.json
+sed -i'' -E -e "s/\"version\": \"[^\"]+\"/\"version\": \"$NEW_VERSION\"/g" package.json
+
+# Run `npm version $version`
+npm version "$NEW_VERSION"
+
+# Generate release notes
+REPOSITORY_URL=https://github.com/mattlong/github-actions-tests
+RELEASE_NOTES=$(echo -e "## What's Changed\n" && git log $(git describe --tags --abbrev=0)..HEAD --pretty=format:'* %s' --reverse && echo -e "\n\n**Full Changelog**: $REPOSITORY_URL/compare/v$CURR_VERSION...v$NEW_VERSION")
+
+# Add changes to the staging area
+git add -u .
+
+# Commit the changes with the release notes
+git commit -s -m "Release Version $NEW_VERSION" -m "$RELEASE_NOTES"
+
+# Push the changes to the remote branch
+git push origin "$BRANCH_NAME"
+
+# Create pull request
+# gh pr create --title "Release Version $NEW_VERSION" --body "$RELEASE_NOTES" --base "$GITHUB_REF_NAME"
+
+# Create draft release
+# gh release create "v$NEW_VERSION" --notes "$RELEASE_NOTES" --title "Version $NEW_VERSION" --draft --latest=$IS_LATEST
