25e6155ef360eb3ea40c939bb9c4ef3f9bd24d9b
Matt Long <ml@medplum.com>
2025-12-12 13:12:47 -0800

    Detect special releases

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 set -xe
 
-npm publish --provenance --access public
+TAG_FLAG=''
+if [ ! -z "$1" ]; then
+  TAG_FLAG="--tag $1"
+  echo "Using tag: $TAG_FLAG"
+fi
+
+npm publish --provenance --access public $TAG_FLAG
