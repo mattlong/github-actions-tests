17e0a227c74169651c7be574beab592afe194f19
Matt Long <ml@medplum.com>
2025-12-12 11:11:06 -0800

    Update bump.sh

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -5,8 +5,8 @@ NEW_VERSION=$1
 
 echo "Bumping to v$NEW_VERSION"
 
-sed -i '' -E -e "s/\"version\": \"[^\"]*\"/\"version\": \"$NEW_VERSION\"/g" package.json
-git add package.json
+npm version "$NEW_VERSION"
+git add package.json package-lock.json
 
 git commit -m "Bump to v$1"
 git tag "v$1"
